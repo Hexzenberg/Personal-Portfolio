0552512b98b43c1a0a48fbcc5305087442cf39ad
Nikhil Kumar Chaudhary <nkc150802@gmail.com>
2024-07-01 20:34:07 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
                 <a href="https://www.linkedin.com/in/hexzenberg" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="https://github.com/hexzenberg" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a href="mailto:hexzenberg@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
-                {/* <a href="">Resume</a> */}
+                <a href="https://drive.google.com/file/d/1kiyPaEwEwYoNpB6TIx31FP7Q2y-b63cL/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
             </div>
         </div>
     );
